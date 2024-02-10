83a86f6754f70739171f11b828df98d3c05c8d39
userquin <userquin@gmail.com>
2024-02-10 17:43:57 +0100

    chore: add pwa-assets branch

--- vite.config.ts
+++ vite.config.ts
@@ -15,29 +15,9 @@ export default defineConfig({
         name: 'Vite PWA',
         short_name: 'Vite PWA',
         theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'maskable-icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
+      },
+      pwaAssets: {
+        config: true,
       },
       devOptions: {
         enabled: true,
